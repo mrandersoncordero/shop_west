a8b00164671718a4f9e285266a831dc3e0c47c26
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-04 15:21:16 -0400

    creacion de nav para el proyecto shop

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,147 @@
+<nav class="navbar">
+  <div class="top-navbar">
+      <div>
+          <span><a href="#"><i class="fa-solid fa-envelope"></i> info@pegoccidente.com</a></span>
+          <span><a href="#"><i class="fa-brands fa-whatsapp"></i> +58 416 - 4567084</a></span>
+      </div>
+      <div>
+          <a href="#"><i class="fa-brands fa-facebook"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram"></i></a>
+          <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+      </div>
+  </div>
+  <div class="navbar-content">
+      <a href="index.html"><img src="{{ asset('images/logo.png') }}" alt="" class="nav-img"></a>
+      <ul class="ul">
+          <li><a href="#" class="underline">Nosotros</a></li>
+          <li><a href="#" class="underline">Proyectos</a></li>
+          <li><a href="#" class="underline">Productos</a>
+              <div class="submenu">
+                  <div class="submenu-categorias">
+                      <a href="#" class="title-category">Linea de Pegamentos <i class="fa-solid fa-angles-right"></i></a>
+                      <div class="submenu-subcategoria">
+                          <div>
+                              <header><a href="#" class="title-subcategory">Basicos <i class="fa-solid fa-angle-right"></i></a></header>
+                              <ul>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Pego Standard Gris</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Pego Extra Blanco</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Premiun</a></li>
+                              </ul>
+                          </div>
+                          <div>
+                              <header><a href="#" class="title-subcategory">Profesional <i class="fa-solid fa-angle-right"></i></a></header>
+                              <ul>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Pego Premiun Gris Grueso</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Super Extra Blanco</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Super Standard Gris</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Super Extra Porcelanato</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Super Extra Piscina</a></li>
+                              </ul>
+                          </div>
+                          <div>
+                              <header><a href="#" class="title-subcategory">Flexible <i class="fa-solid fa-angle-right"></i></a></header>
+                              <ul>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Pego Supremo Blanco</a></li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="submenu-categorias">
+                      <a href="#" class="title-category">Linea de Construccion <i class="fa-solid fa-angles-right"></i></a>
+                      <div class="submenu-subcategoria">
+                          <div>
+                              <header><a href="#" class="title-subcategory">Revestimiento <i class="fa-solid fa-angle-right"></i></a></header>
+                              <ul>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Occifriso</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Occifriso AR</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Occifriso Proyectado</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Occifriso Texturizado</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Occimix</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Stuco</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Imperplus</a></li>
+                              </ul>
+                          </div>
+                          <div>
+                              <header><a href="#" class="title-subcategory">Pegamentos <i class="fa-solid fa-angle-right"></i></a></header>
+                              <ul>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Occibloque</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Occiteja</a></li>
+                              </ul>
+                          </div>
+                          <div>
+                              <header><a href="#" class="title-subcategory">Estructural <i class="fa-solid fa-angle-right"></i></a></header>
+                              <ul>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Occiconcreto</a></li>
+                                  <li><a href="#"><ion-icon name="radio-button-on-outline" class="icon-diana"></ion-icon>Occiconcreto Reforzado</a></li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="submenu-categorias">
+                      <a href="#" class="title-category">Linea de Sella Juntas <i class="fa-solid fa-angles-right"></i></a>
+                      <div class="submenu-subcategoria">
+                          <ul>
+                              <li><a href="#"><ion-icon name="radio-button-on-outline"></ion-icon>D' Color</a></li>
+                              <li><a href="#"><ion-icon name="radio-button-on-outline"></ion-icon>D' Color Marmolina</a></li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </li>
+          <li><a href="#" class="underline">Contacto</a></li>
+      </ul>
+      <div class="icon-nav">
+          <div>
+              <form action="" class="form-search">
+                  <select name="" class="search-select">
+                      <option value="">Pegamentos</option>
+                      <option value="">Construccion</option>
+                      <option value="">Juntas</option>
+                  </select>
+                  <div class="div-search-input">
+                      <input type="text"  placeholder="Buscar Producto" class="search-input">
+                      <button class="button-search">
+                          <i class="fa-solid fa-magnifying-glass"></i>
+                      </button>
+                  </div>
+              </form>
+          </div>
+          <div>
+              <a href="#"><i class="fa-solid fa-truck"></i></a>
+              <span>7</span>
+          </div>
+          <div class="icon-user">
+              <a href="#"><i class="fa-solid fa-user"></i></a>
+          </div>
+          <div class="icon-bars">
+              <ion-icon name="reorder-four-outline" id="icon-hamburguer"></ion-icon>
+          </div>
+      </div>
+  </div>
+
+
+  <div class="mobile-navbar">
+      <ul>
+          <li><a href="index.html" class="underline">Nosotros</a></li>
+          <li><a href="index.html" class="underline">Proyectos</a></li>
+          <li class="mobile-menu" id="submenu-down">
+              <a href="#" class="line-down underline" id="submenu-down-two">Linea de Pegamentos<i class="fa-solid fa-angle-down" id="angle"></i></ion-icon></a>
+              <ul class="mobile-submenu">
+                  <li><a href="productos.html#2" class="underline">Basicos<i class="fa-solid fa-angle-right"></i></a></li>
+                  <li><a href="productos.html#3" class="underline">Profesional<i class="fa-solid fa-angle-right"></i></a></li>
+                  <li><a href="productos.html#4" class="underline">Flexible<i class="fa-solid fa-angle-right"></i></a></li>
+              </ul>
+          </li>
+          <li class="mobile-menu" id="submenu-down">
+              <a href="#" class="line-down underline" id="submenu-down-two">Linea de Construccion<i class="fa-solid fa-angle-down" id="angle"></i></ion-icon></a>
+              <ul class="mobile-submenu">
+                  <li><a href="productos.html#2" class="underline">Revestimiento<i class="fa-solid fa-angle-right"></i></a></li>
+                  <li><a href="productos.html#3" class="underline">Pegamentos<i class="fa-solid fa-angle-right"></i></a></li>
+                  <li><a href="productos.html#4" class="underline">Estructural<i class="fa-solid fa-angle-right"></i></a></li>
+              </ul>
+          </li>
+          <li><a href="index.html" class="underline">Linea D' Sella Juntas</a></li>
+          <li><a href="index.html#contacto" class="underline">Contacto</a></li>
+      </ul>
+  </div>
+</nav>
