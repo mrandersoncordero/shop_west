a04d10b458d987573d09347cc254314ce03dd688
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-05 15:06:10 -0400

    modificar migracion

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->boolean('is_active');
+            $table->tinyInteger('is_active')->default(0);
             $table->timestamps();
         });
     }
