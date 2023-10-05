1e03164f565b55dcd23f2c6e3edd8a4a537e1548
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-05 14:16:59 -0400

    nuevas imagenes

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,18 @@
 // Controller
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\UserController;
+
 // Laravel
 use Illuminate\Support\Facades\Route;
 
 Route::controller(PageController::class)->group(function() {
     Route::get('/', 'index')->name('index');
 });
+
+Route::resource('categories', CategoryController::class);
+Route::resource('users', UserController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
