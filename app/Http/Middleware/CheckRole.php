d20c92a077f23c16742c946566cd2b4ed591d27e
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-05 15:06:59 -0400

    Middleware para chequear el tipo de usuario

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Spatie\Permission\Traits\HasRoles;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+    if (!auth()->check()) {
+        // El usuario no está autenticado, puedes manejarlo como prefieras
+        return redirect('/login');
+    }
+
+    foreach ($roles as $role) {
+        if (auth()->user()->hasRole($role)) {
+            return $next($request);
+        }
+    }
+
+    abort(403, 'Acceso no autorizado');
+    }
+}
