ec18cbc87faf125559a91a90da13db0e7eec4e8a
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-09 14:55:56 -0400

    usar template de dashboard

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,21 +1,30 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Posts') }}
-        </h2>
-    </x-slot>
+@extends('templates.dashboard')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action="{{ route('categories.update', $category) }}" method="post">
-                        @method('PUT')
-          
-                        @include('admin.category._form')
-                    </form>
-                </div>
-            </div>
-        </div>
+@section('head_content')
+<title>Dashboard</title>
+@endsection
+
+@section('content')
+<div class="home_content">
+    <div class="header_container">
+        <h1 class="text">Edit category "{{ $category->name }}"</h1>
     </div>
-</x-app-layout>
+    <form action="{{ route('categories.update', $category) }}" method="post">
+        @csrf
+        @method('PUT')
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" name="name" value="{{ $category->name }}">
+            <label for="floatingInput">Name</label>
+        </div>
+        
+        <div class="form-floating">
+            <textarea class="form-control" name="description" rows="5">{{ $category->description }}</textarea>
+            <label for="floatingTextarea">Description</label>
+        </div>
+
+        <div style="display: flex; align-items: center; margin-top: 10px;">
+            <input type="submit" value="Enviar" class="btn btn-primary">
+        </div>
+    </form>
+</div>   
+@endsection
