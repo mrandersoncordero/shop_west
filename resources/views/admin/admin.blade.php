035b94195caae409552b39732b192e79b3cb0975
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-09 11:21:21 -0400

    cambair nombre del archivo

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.dashboard')
+
+@section('head_content')
+<title>Dashboard</title>
+@endsection
+
+@section('content')
+<div class="home_content">
+    <div class="text">Home Content</div>
+    Admin Dashboard
+</div>
+@endsection
