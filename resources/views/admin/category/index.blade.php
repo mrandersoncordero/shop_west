0edc8a962816d5418202707b201cb9ebad4a1d6e
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-09 14:55:43 -0400

    cambios del index en categorias

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -6,23 +6,31 @@
 
 @section('content')
 <div class="home_content">
-    <div class="text">Home Content</div>
-    <table class="mb-4">
+    <div class="header_container">
+        <h1 class="text">Categories and subcategories</h1>
+        <button id="buttonModal" class="btn btn-primary">Crear</button>
+    </div>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">description</th>
+              <th scope="col">Actions</th>
+            </tr>
+        </thead>
     @forelse ($categories as $category)
-        <tr class="border-b border-gray-200 text-sm">
-            <td class="px-6 py-4">{{ $category->name }}</td>
-            <td class="px-6 py-4">{{ $category->description }}</td>
-            <td class="px-6 py-4">
-                <a href="{{ route('categories.edit', $category) }}" class="text-indigo-600">Editar</a>
-            </td>
+        <tr class="">
+            <td class="">{{ $category->name }}</td>
+            <td class="">{{ $category->description }}</td>
             <td>
+                <a href="{{ route('categories.edit', $category) }}" class="edit">Editar</a>
                 <form action="{{ route('categories.destroy', $category) }}" method="POST">
                 @csrf
                 @method('DELETE')
                 <input 
                     type="submit" 
                     value="Eliminar" 
-                    class="bg-gray-800 text-white rounded px-4 py-2"
+                    class=""
                     onclick="return confirm('Desea Eliminar?')"
                 >
                 </form>
@@ -34,5 +42,20 @@
         </div>
     @endforelse
   </table>
+
+
 </div>
+<div id="modal" class="modal-container">
+    <div class="modal_content">
+        <div class="modal_header">
+            <div>
+                <p>Crear categoria</p>
+                <i class='bx bx-x' id="close"></i>
+            </div>
+        </div>
+        <form action="{{ route('categories.store') }}" method="post" class="mt-4">
+            @include('admin.category._form')
+        </form>
+    </div>
+</div>    
 @endsection
