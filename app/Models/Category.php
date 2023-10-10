6531eead30184ce4ad874eabf85226b6343afe2c
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-10 16:16:05 -0400

    establecer relacion con productos productos

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -20,4 +20,9 @@ class Category extends Model
     {
         return $this->hasMany(Subcategory::class);
     }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
