a97ea4322d556d179616ad47988faef21a94b235
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-11 16:11:12 -0400

    cambiar ruta del archivo y tomar 5 productos

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('index');
+        return view('page.index', [
+            'products' => Product::take(5)->get(),
+        ]);
     }
 }
