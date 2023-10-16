c8c6ae4f697c1658b11dcfe32cd7ec1cb38f46ca
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-16 09:01:24 -0400

    cambiar la estructura y estilos de la vista principal

--- resources/views/page/index.blade.php
+++ resources/views/page/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('head_content')
 <title>Pegoccidente - Shop</title>
+<link rel="stylesheet" href="{{ asset('css/glide.core.min.css') }}">
+<!-- Optional Theme Stylesheet -->
+<link rel="stylesheet" href="{{ asset('css/glide.theme.min.css') }}">
+
 @endsection
 
 @section('content')
@@ -10,27 +14,29 @@
 </div>
   
 <main>
-    {{-- <aside class="primary-banner">
-        <img src="{{ asset('images/planta-barquisimeto.png') }}" alt="">
-        <h1 class="h1">Tu proveedor confiable de materiales de construcción y adhesivos de calidad</h1>
-    </aside> --}}
-    
+   
     <section class="product-section">
         <header>
             <h1 class="h2"><i class="fa-solid fa-quote-left quotes"></i>Productos confiables de construcción y adhesivos<i class="fa-solid fa-quote-right quotes"></i></h1>
         </header>
         <article class="productos-destacados">
             <div class="content">
-                @foreach ($products as $item)
-                <header>
-                    <h2 class="titulo-producto-destacado">{{ $item->name }}</h2>
-                </header>
-                <p id="descripcion-producto">
-                    {{ $item->description }} 
-                </p>
-                @break
-                @endforeach
-                <a href="">IR A PRODUCTOS</a>
+                <div class="titulo-destacado">
+                    <div><i class="fa-solid fa-star"></i></div>
+                    <h2>Destacados</h2>
+                </div>
+                <div>
+                    @foreach ($products as $item)
+                    <header>
+                        <h3 class="titulo-producto-destacado">{{ $item->name }}</h3>
+                    </header>
+                    <p id="descripcion-producto">
+                        {{ $item->description }} 
+                    </p>
+                    @break
+                    @endforeach
+                    <a href="">IR A PRODUCTOS</a>
+                </div>
             </div>
             <div class="diana-de-productos">
                 <div class="diana-container">
@@ -51,10 +57,90 @@
             </div>
         </article>
     </section>
+
+    <div class="banner">
+        <img src="{{ asset('images/banner_pego.png') }}" alt="" class="banner_image">
+    </div>
+
+    <section class="categories">
+        <header>
+            <h1 class="h2"><i class="fa-solid fa-quote-left quotes"></i>Categorias<i class="fa-solid fa-quote-right quotes"></i></h1>
+        </header>
+        <article class="container-categories">
+            <div class="category">
+                <a href="#" style="background: url({{asset('images/categoriy_pegamento.jpg')}})"></a>
+                <p>
+                    <a href="#" style="color: var(--blue)">Linea de pegamentos</a>
+                </p>
+            </div>
+            <div class="category">
+                <a href="#" style="background: url({{asset('images/category_construccion.jpg')}})"></a>
+                <p>
+                    <a href="#" style="color: var(--red)">Linea de construcción</a>
+                </p>
+            </div>
+            <div class="category">
+                <a href="#" style="background: url({{asset('images/category_sella_juntas.jpg')}})"></a>
+                <p>
+                    <a href="#" style="color: var(--blue)">Linea de sella juntas</a>
+                </p>
+            </div>
+        </article>
+    </section>
+
+    <section class="projects">
+        <header>
+            <h2 class="h2"><i class="fa-solid fa-quote-left quotes"></i>Proyectos<i class="fa-solid fa-quote-right quotes"></i></h2>
+        </header>
+        <div class="container">
+            <div class="glide" style="position: relative">
+    
+                <div class="glide__track" data-glide-el="track">
+                    <ul class="glide__slides">
+                        <li class="glide__slide">
+                            <img src="{{ asset('images/projects/lidotel.png') }}" alt="" style="width: 100%; height: 300px;">
+                        </li>
+                        <li class "glide__slide">
+                            <img src="{{ asset('images/projects/catedral.jpg') }}" alt="" style="width: 100%; height: 300px;">
+                        </li>
+                        <li class="glide__slide">
+                            <img src="{{ asset('images/projects/parque_la_musica.png') }}" alt="" style="width: 100%; height: 300px;">
+                        </li>
+                    </ul>
+                </div>
+    
+                <div class="glide" style="position: initial">
+    
+                    <div class="glide__arrows" data-glide-el="controls">
+                        <button class="glide__arrow glide__arrow--left" data-glide-dir="<">prev</button>
+                        <button class="glide__arrow glide__arrow--right" data-glide-dir=">">next</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
+
+@include('templates.footer')
 
 <script>
     let products = @json($products);
 </script>
 <script src="{{ asset('js/productos_destacados.js') }}"></script>
+<script src="{{ asset('js/glide.min.js') }}"></script>
+<script>
+    const config = {
+        type: 'carousel',
+        perView: 3,
+        breakpoints: {
+            1024: {
+                perView: 2
+            },
+            600: {
+                perView: 1
+            }
+        }
+    };
+    new Glide('.glide', config).mount()
+</script>
 @endsection
