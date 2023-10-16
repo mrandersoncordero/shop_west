8d393c8193f4180e9c93c659f66b21412b70cbf7
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-16 09:00:30 -0400

    copiar los archivos de glide a public

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,15 @@ export default defineConfig({
                 'resources/js/app.js',
             ],
             refresh: true,
+        }),
+
+        copy({
+            targets: [
+                // Copia los archivos necesarios de Glide.js desde node_modules a tu carpeta pública
+                { src: 'node_modules/@glidejs/glide/dist/css/glide.core.min.css', dest: 'public/css' },
+                { src: 'node_modules/@glidejs/glide/dist/css/glide.theme.min.css', dest: 'public/css' },
+                { src: 'node_modules/@glidejs/glide/dist/glide.min.js', dest: 'public/js' },
+            ],
         }),
     ],
 });
