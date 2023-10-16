3a2097d73f06f9514928852fc4632e46607dae44
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-16 15:40:44 -0400

    footer

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,28 @@
+<footer>
+  <section class="container_footer">
+      <article class="footer_section_primary">
+          <img src="{{ asset('images/logoblanco.png') }}" alt="Logo blanco">
+      </article>
+      <article class="footer_section_secondary">
+          <div class="rights">
+              <p>Pegoocidente - Todos los derechos reservados &copy; desarrollado por Branar, C.A.</p>
+          </div>
+          <div class="container_socials">
+              <ul>
+                  <li>
+                      <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                  </li>
+                  <li>
+                      <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                  </li>
+                  <li>
+                      <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+                  </li>
+                  <li>
+                      <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                  </li>
+              </ul>
+          </div>
+      </article>
+  </section>
+</footer>
