e744ab6f456ca5eb7d8ebbf9c88d412679fae85f
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-16 15:39:49 -0400

    nueva ruta de products

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(PageController::class)->group(function () {
     Route::get('/', 'index')->name('index');
+    Route::get('/products', 'products_view')->name('products_view');
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
