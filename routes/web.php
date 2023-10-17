c4eced59527bbcb1f4a85a6a71d10d3467be51d2
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-17 14:31:19 -0400

    ruta para detalle de producto

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(PageController::class)->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('/products', 'products_view')->name('products_view');
+    Route::get('/products/article/{id}', 'product_detail')->name('product_detail');
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
