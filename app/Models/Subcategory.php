3f1d382bc9c1ef7aef52849fa0e1c9378ab98d0a
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-18 15:45:55 -0400

    establecer relacion de metodos con Product

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -21,4 +21,9 @@ class Subcategory extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
