2412fc8b4df793d49b40839fc0f280a91a23ac8e
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-10-20 15:11:09 -0400

    agregar script al template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -20,6 +20,7 @@
   
 
   <script src="{{ asset('js/navbar.js') }}"></script>
+  <script src="{{ asset('js/menu.js') }}"></script>
   <!-- IONICONS -->
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
