f683a659659df5fadd2cf27720756c6c6bd0f957
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-01 09:36:01 -0400

    otro uso de datatable

--- public/js/tables_.js
+++ public/js/tables_.js
@@ -4,3 +4,10 @@ let table = new DataTable("#myTable", {
         { targets: [0, 1, 2, 3], searchable: true }
     ]
 });
+
+let table_detail_order = new DataTable("#table_detail_order", {
+    paging: false, // Deshabilita la paginación
+    info: false,   // Deshabilita la información de la paginación
+    searching: false, // Deshabilita la búsqueda
+    lengthChange: false // Deshabilita la opción de mostrar una cantidad personalizada de elementos por página
+});
