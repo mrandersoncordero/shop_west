44a53a8bf66858d5aeeba3b1f94af7a25f8814b2
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-02 15:59:11 -0400

    vista detalle de la orden

--- resources/views/order/edit.blade.php
+++ resources/views/order/edit.blade.php
@@ -4,7 +4,7 @@
 <title>Pegoccidente - My orders</title>
 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 @endsection
 
@@ -16,6 +16,7 @@
 
   <section class="container_order_buy">
     <article class="data_user">
+      {{-- Informacion del usuario --}}
       <p>
         <b>Nombre o razon social:</b> {{ $order->user->profile->first_name }}
       </p>
@@ -31,6 +32,58 @@
       <p>
         <b>Correo electronico:</b> {{ $order->user->email }}
       </p>
+
+      @if ($order->status->id == 1)
+
+        @if (($order->payment_type_id == 3 || $order->payment_type_id == 4) && $isset_payment->isEmpty() )
+          <form action="{{ route('payment.store') }}" method="post" class="container border rounded-2 p-4 mt-4" enctype="multipart/form-data">
+            @csrf
+            <label for="" class="form-label">Registrar comprobante de pago</label>
+            <input type="hidden" name="order_id" value="{{ $order->id }}">
+            @error('order_id')
+            <div class="invalid-feedback">
+              {{ $message }}
+            </div> 
+            @enderror
+
+            <div class="form-floating mb-3">
+              <input type="text" id="floating_bank_reference" class="form-control @error('bank_reference') is-invalid @enderror" name="bank_reference" value="{{ old('bank_reference','')}}">
+              <label for="floating_bank_reference">Referencia bancaria</label>
+              @error('bank_reference')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div> 
+              @enderror
+            </div>
+
+            <div class="mb-3">
+              <label for="formFile" class="form-label">Archivo</label>
+              <input class="form-control @error('file') is-invalid @enderror" type="file" id="formFile" name="file">
+              @error('file')
+              <div class="invalid-feedback">
+                {{ $message }}
+              </div> 
+              @enderror
+            </div>
+            <input type="submit" value="Enviar">
+          </form>
+        @else
+          <div class="container mt-4">
+            <p>Estamos comprobando tu pago, uno de nuestros administradores se pondra en contacto contigo al comprobar dicho pago.</p>
+            <div>
+              <p>El metodo de pago de esta orden de compra es: <b>{{ $order->payment_type->name}}</b></p>
+              @foreach ($isset_payment as $item)
+              <p><b>Referencia bancaria:</b> {{$item->bank_reference }}</p>
+              <a href="{{ asset("proof_of_payment/$item->file" )}}">comprobante de pago</a>
+              @endforeach
+            </div>
+          </div>
+        @endif
+
+      @else
+        <p>Su pedido esta siendo revisado</p>
+      @endif
+
     </article>
     <article class="data_order_product">
       <table id="table_detail_order" class="display table table-bordered">
@@ -101,7 +154,9 @@
 
           // Formatea las variables para limitar a 2 decimales
           $iva = number_format($iva, 2);
-          $igtf = number_format($igtf, 2);
+          if($order->payment_type_id == 2) {
+            $igtf = number_format($igtf, 2);
+          }
           $total_price = number_format($total_price, 2);
           @endphp
           <tr>
@@ -134,5 +189,7 @@
 @section('scripts')
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 <script type="module" src="{{ asset('js/tables_.js') }}"></script>
 @endsection
