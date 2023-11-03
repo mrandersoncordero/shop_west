390ecea2445f2092cae82f8d7006291377f213d1
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-03 11:39:19 -0400

    controlador para enviar correo bienvenida  a user

---
+++ app/Mail/WelcomeMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Bienvenido a la tienda Productos Occidente') // Define el asunto del correo
+                    ->view('emails.welcome'); // Define la vista del correo
+    }
+}
