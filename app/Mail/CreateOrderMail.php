2b1d34928e1b94fabd33b182d26ad41022d5d808
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-06 16:25:55 -0400

    controller ORder

---
+++ app/Mail/CreateOrderMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class CreateOrderMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $order;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user, Order $order)
+    {
+        $this->user = $user;
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject("Orden creada por el usuario {$this->user->profile->full_name()}") // Define el asunto del correo
+                    ->view('emails.create_order'); // Define la vista del correo
+    }
+}
