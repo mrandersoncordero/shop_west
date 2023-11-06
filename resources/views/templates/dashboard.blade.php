8d60292c15c7283003d7775da69d37dddc0c6880
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-06 15:26:09 -0400

    corregir error

--- resources/views/templates/dashboard.blade.php
+++ resources/views/templates/dashboard.blade.php
@@ -22,11 +22,10 @@
 </body>
 
 
-<script>
+
   <script src="{{ asset('js/nav_dashboard.js') }}"></script>
   <script src="{{ asset('js/modal.js') }}"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-@yield('scripts')
-</script>
+  @yield('scripts')
 </html>
