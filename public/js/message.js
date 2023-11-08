dff9d2cd982c0733d380bff1476dd3d09ee19e2b
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-08 15:00:30 -0400

    eliminar message

--- public/js/message.js
+++
@@ -1,7 +0,0 @@
-const container_message = document.querySelector('.container_message');
-const close_message = document.querySelector('#close_message');
-
-close_message.addEventListener('click', function(e) {
-  container_message.classList.add('hidden');
-  console
-});
