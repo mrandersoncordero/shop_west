319bb415cfb37a52a359ea7a2272df6c72fc8590
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-10 11:55:30 -0400

    creacion de las alertas

---
+++ resources/views/layouts/partials/alert.blade.php
@@ -0,0 +1,9 @@
+<div class="content_alert {{session('message')['class']}}">
+  <div class="layer"></div>
+  <h4>{{session('message')['title']}}</h4>
+  <div>
+    <p>
+      {{session('message')['content']}}
+    </p>
+  </div>
+</div>
