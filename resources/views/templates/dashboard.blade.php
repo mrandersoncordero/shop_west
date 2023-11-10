d5930bdd7b6dde9f9c2696e2a4fe0145e22200e4
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-10 11:56:21 -0400

    modificar el template del dashboard

--- resources/views/templates/dashboard.blade.php
+++ resources/views/templates/dashboard.blade.php
@@ -7,6 +7,7 @@
   
   <link rel="stylesheet" href="{{ asset('css/nav_dashboard.css') }}">
   <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/alert.css') }}">
 
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -16,15 +17,24 @@
 <body>
 
   @include('templates.navigation')
-  @yield('content')
   
+  <main class="home_content">
+  @if (session('message'))
+  @include('layouts.partials.alert')
+  @endif
 
+  @yield('content_primary')
+
+  </main>
+
+  @yield('content_secondary')
 </body>
 
 
 
   <script src="{{ asset('js/nav_dashboard.js') }}"></script>
   <script src="{{ asset('js/modal.js') }}"></script>
+  <script src="{{ asset('js/alert.js') }}"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
   @yield('scripts')
