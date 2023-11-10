b18ee3e1e711fd71b2d1114543d762644b0b7f22
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-10 16:09:19 -0400

    lang es

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '¡Su contraseña ha sido restablecida!',
+    'sent' => '¡Recordatorio de contraseña enviado!',
+    'token' => 'Este token de restablecimiento de contraseña es inválido.',
+    'user' => 'No se ha encontrado un usuario con esa dirección de correo.',
+    'throttled' => 'Por favor espere antes de volver a intentarlo.',
+    'password' => 'Las contraseñas deben tener al menos seis caracteres y coincidir con la confirmación.',
+    'intro' => 'Estás recibiendo este email porque se ha solicitado un cambio de contraseña para tu cuenta.',
+    'link_expire' => 'Este enlace de restablecimiento de contraseña caducará en :count minutos.',
+    'no_action' => 'Si no has solicitado un restablecimiento de contraseña, no es necesario realizar ninguna otra acción.',
+
+];
