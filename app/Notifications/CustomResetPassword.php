a20002d91e4ff54ffeb05ff6ffdbdecf41fa47fc
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-10 16:08:13 -0400

    Notificacion de Reset Password

---
+++ app/Notifications/CustomResetPassword.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Notifications\Notification;
+
+class CustomResetPassword extends Notification
+{
+    use Queueable;
+
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)->view('mails.reset', ['token' => $this->token])
+        ->greeting('Hola que tal')
+        ->subject(Lang::get('Reset Password Notification'))
+        ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
+        ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+        ->line(Lang::get('If you did not request a password reset, no further action is required.'))
+        ->action(Lang::get('Reset Password'), route('password.reset', ['token' => $this->token]));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
