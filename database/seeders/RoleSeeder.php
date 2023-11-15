73f5e782287e902fdb9aeb998cd9d75d89edea1e
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-15 10:20:58 -0400

    crear roles con el modelo Role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -13,7 +13,6 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
         Role::create(['name' => 'superuser']);
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'client']);
