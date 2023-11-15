813dce923dfd973781d2823786b0e6526fab69f0
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-15 10:20:26 -0400

    modificar el middleware de checkRole

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Role;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 use Spatie\Permission\Traits\HasRoles;
@@ -17,19 +19,20 @@ class CheckRole
      */
     public function handle($request, Closure $next, ...$roles)
     {
-    if (!auth()->check()) {
-        // El usuario no está autenticado, puedes manejarlo como prefieras
-        return redirect('/login');
-    }
-
-    foreach ($roles as $role) {
-        if (auth()->user()->hasRole($role)) {
-            return $next($request);
-        }else{
-            return redirect('/');
+        if (!Auth::check()) {
+            // El usuario no está autenticado, puedes manejarlo como prefieras
+            return redirect('/login');
         }
-    }
 
-    abort(403, 'Acceso no autorizado');
+        
+        $data_role = explode("|", $roles[0]);
+        foreach ($data_role as $role) {
+            if (Auth::user()->role->name === $role) {
+                return $next($request);
+            }
+        }
+        
+
+        abort(403, 'Acceso no autorizado');
     }
 }
