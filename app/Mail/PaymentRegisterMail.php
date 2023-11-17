a03dc3f0b2f48e6485f4bad9f1a01f9cf8a5ab68
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-11-16 20:04:59 -0400

    controlador para enviar correo de registro de pago

---
+++ app/Mail/PaymentRegisterMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Order;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PaymentRegisterMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $order;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param User $user
+     */
+    public function __construct(User $user, Order $order)
+    {
+        $this->user = $user;
+        $this->order = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject("Anexo de pago") // Define el asunto del correo
+                    ->view('mails.register_payment'); // Define la vista del correo
+    }
+}
