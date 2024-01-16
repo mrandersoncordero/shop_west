b779aa502f1ba94696fb0b60dc2defa2245910fb
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-01-16 16:28:04 -0400

    migration, controller and model for interested_clients

---
+++ app/Http/Controllers/InterestedClientController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\CatalogMail;
+use App\Models\InterestedClient;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class InterestedClientController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|email',
+            'accept' => 'boolean',
+        ]);
+
+        // Verificar si el correo ya existe
+        $existingClient = InterestedClient::where('email', $request->input('email'))->first();
+        
+        if ($existingClient) {
+            // Enviar correo si el cliente ya existe
+            // Puedes personalizar esta lógica según tus necesidades
+            // Por ejemplo, puedes enviar un recordatorio o una oferta especial.
+            Mail::to($existingClient->email)->send(new CatalogMail($existingClient));
+        } else {
+            // Crear un nuevo cliente solo si no existe
+            $interestedClient = InterestedClient::create($request->all());
+
+            // Envía el correo si deseas hacerlo también para nuevos clientes
+            Mail::to($interestedClient->email)->send(new CatalogMail($interestedClient));
+        }
+
+        return redirect()->back()->with('message', [
+            'class' => 'alert--success',
+            'title' => 'Envio de catalogo',
+            'content' => "Se ha enviado el catalogo al correo proporcionado"
+        ]);
+    }
+
+}
---
+++ app/Mail/CatalogMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\InterestedClient;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class CatalogMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $interestedClient;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param User $user
+     */
+    public function __construct( InterestedClient $interestedClient)
+    {
+        $this->interestedClient = $interestedClient;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        
+        return $this->subject("Catálogo de Productos") // Define el asunto del correo
+                ->to($this->interestedClient->email) 
+                ->view('mails.catalog_mail'); // Define la vista del correo
+        
+    }
+}
