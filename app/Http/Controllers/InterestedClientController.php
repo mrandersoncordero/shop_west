f9df72b7c0d0df8804f700eb0d37afc2d8b421de
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-01-18 10:59:00 -0400

    cambiar logica del ennvio de correo

--- app/Http/Controllers/InterestedClientController.php
+++ app/Http/Controllers/InterestedClientController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Mail\CatalogMail;
+use App\Models\Product;
 use App\Models\InterestedClient;
+use App\Mail\ProductSheet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -23,17 +25,46 @@ class InterestedClientController extends Controller
         // Verificar si el correo ya existe
         $existingClient = InterestedClient::where('email', $request->input('email'))->first();
         
-        if ($existingClient) {
-            // Enviar correo si el cliente ya existe
-            // Puedes personalizar esta lógica según tus necesidades
-            // Por ejemplo, puedes enviar un recordatorio o una oferta especial.
-            Mail::to($existingClient->email)->send(new CatalogMail($existingClient, $clientData));
-        } else {
-            // Crear un nuevo cliente solo si no existe
-            $interestedClient = InterestedClient::create($request->all());
+        switch ($request->input('name_mail')) {
+            case 'catalog':
+                if ($existingClient) {
+                    // Enviar correo si el cliente ya existe
+                    // Puedes personalizar esta lógica según tus necesidades
+                    // Por ejemplo, puedes enviar un recordatorio o una oferta especial.
+                    Mail::to($existingClient->email)->send(new CatalogMail($existingClient, $clientData));
+                } else {
+                    // Crear un nuevo cliente solo si no existe
+                    $interestedClient = InterestedClient::create($request->all());
+        
+                    // Envía el correo si deseas hacerlo también para nuevos clientes
+                    Mail::to($interestedClient->email)->send(new CatalogMail($interestedClient, $clientData));
+                }
+                break;
+            
+            case 'sheet':
+                // Obtenemos los datos del producto
+                $product = Product::where('id', $request->input('product'))->first();
 
-            // Envía el correo si deseas hacerlo también para nuevos clientes
-            Mail::to($interestedClient->email)->send(new CatalogMail($interestedClient, $clientData));
+                if ($existingClient) {
+                    // Enviar correo si el cliente ya existe
+                    // Puedes personalizar esta lógica según tus necesidades
+                    // Por ejemplo, puedes enviar un recordatorio o una oferta especial.
+                    Mail::to($existingClient->email)->send(new ProductSheet($product, $clientData));
+                } else {
+                    // Crear un nuevo cliente solo si no existe
+                    $interestedClient = InterestedClient::create($request->all());
+        
+                    // Envía el correo si deseas hacerlo también para nuevos clientes
+                    Mail::to($interestedClient->email)->send(new ProductSheet($product, $clientData));
+                }
+                break;
+            default:
+                return redirect()->back()->with('message', [
+                    'class' => 'alert--danger',
+                    'title' => 'Envio de catalogo',
+                    'content' => "Deja de joder"
+                ]);
+                break;
         }
 
         return redirect()->back()->with('message', [
