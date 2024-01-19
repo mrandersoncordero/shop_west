2a25f71741f5405fe884fa525aaa05117159b572
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-01-19 13:50:18 -0400

    controlador del formulario de contacto

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Mail\Contact;
+use App\Mail\CompanyContact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    
+    public function send_email(Request $request) {
+        $request->validate([
+            'complete_name' => 'nullable|string',
+            'email' => 'required|email',
+            'phone_number' => 'nullable|regex:/^[0-9]+$/|max:15', // Validación de solo números y máximo 15 caracteres
+            'subject' => 'nullable|string',
+            'comment' => 'nullable|string|max:255', // Limitar a 255 caracteres
+        ]);
+
+        $clientData = $request->all();
+
+        // Dirección de correo electrónico de la empresa
+        $companyEmail = 'acorderofigueroa7@gmail.com';
+
+        // Enviar correo a la empresa
+        Mail::to($companyEmail)
+            ->send((new CompanyContact($clientData))
+            ->replyTo($request->input('email')));
+
+        // Enviar correo a la empresa
+        Mail::to($companyEmail)
+            ->send(new Contact($clientData));
+
+        return redirect()->back()->with('message', [
+            'class' => 'alert--success', // Cambiado a éxito para indicar que el correo se envió correctamente
+            'title' => 'Envio de correo de contacto',
+            'content' => 'El correo se envió correctamente. Pronto nos pondremos en contacto contigo.',
+        ]);
+    }
+}
