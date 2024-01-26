a95e7504b41e1da0f66dee62880d2ff37ee7402b
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-01-26 14:11:23 -0400

    comentar linea del favicon

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   {{-- Favicon --}}
-  <link rel="icon" href="{{ asset('icons/diana.ico') }}" type="image/x-icon">
+  {{-- <link rel="icon" href="{{ asset('icons/diana.ico') }}" type="image/x-icon"> --}}
 
   {{-- Metaetiquetas para redes sociales  --}}
   <meta property="og:image" content="{{ asset('icons/diana.ico') }}">
