4c0d9e0c1b6fcdc853bda41b91e775e5c2b3b98a
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-02-15 10:38:04 -0400

    incluir nuevos archivos

--- resources/views/page/product_detail.blade.php
+++ resources/views/page/product_detail.blade.php
@@ -4,6 +4,8 @@
   <title>Pegoccidente - Article {{ $product->name }}</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/zoom.js/1.7.21/zoom.min.js"></script>
 @endsection
 
 @section('content')
@@ -52,7 +54,7 @@
           <div class="diana_detail--primary">
             <img src="{{ asset("images/sello_de_garantia.png" )}}" class="sello_garantia" alt="sello de garantia">
             <div class="diana_detail--secondary">
-              <img src="{{ asset("product/$product->image" )}}" alt="">
+              <img src="{{ asset("product/$product->image" )}}"  alt="">
             </div>
           </div>
         </section>
@@ -116,60 +118,32 @@
                 @endfor
             </div>
           </div>
-          <button type="button" class="btn" style="background-color: var(--blue); color: #fff;" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            Obtener ficha del producto
-        </button>
-          
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h1 class="modal-title fs-5" id="exampleModalLabel">Ingresa los datos</h1>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form  action="{{ route('interested-clients.store') }}" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                          <label for="first_name" class="form-label">Nombre</label>
-                          <input type="text" name="first_name" class="form-control" id="first_name">
-                        </div>
-                        <div class="mb-3">
-                          <label for="last_name" class="form-label">Apellido</label>
-                          <input type="text" name="last_name" class="form-control" id="last_name">
-                        </div>
-                        <div class="mb-3">
-                          <label for="email" class="form-label">Correo</label>
-                          <input type="email" name="email" class="form-control" id="email">
-                        </div>
-                        <div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="accept" value="1">
-                            <label class="form-check-label" name="accept" for="accept">Aceptas recibir promociones y descuentos?</label>
-                        </div>
-                        
-                        <input type="hidden" name="name_mail" value="sheet">
-                        <input type="hidden" name="product" value="{{ $product->id }}">
-                        <button type="submit" class="btn" style="background-color: var(--blue); color: #fff;">Enviar</button>
-                    </form>
-                </div>
-              </div>
-            </div>
-        </div>
-          <!-- Agrega tu función de JavaScript -->
-          <script>
-              // Función para pintar las estrellas
-              function paintStars(rating) {
-                  const stars = document.querySelector('.star-rating');
-                  const percentage = (rating / 5) * 100; // Convertir la calificación en un porcentaje
-                  console.log(percentage);
-                  //stars.style.width = `${percentage}%`; // Establecer el ancho del contenedor de estrellas
-              }
+        
+          <div style="display: flex; align-items: center; gap: 4px;"> 
+            <button type="button" class="btn" style="background-color: var(--blue); color: #fff;" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              Obtener ficha del producto
+            </button>
+            @include('page.partials.modal_form_tab')
+            
+            <button type="button" class="btn" style="background-color: var(--blue); color: #fff;" data-bs-toggle="modal" data-bs-target="#ModalPaleta">
+              Paleta de colores
+            </button>
+            @include('page.partials.paleta_colores')
+          </div>
 
-              // Obtener el promedio de calificación del contenedor y pintar las estrellas
-              const averageRating = parseFloat(document.querySelector('.star-rating').getAttribute('data-rating'));
-              paintStars(averageRating);
-          </script>
+        <!-- Agrega tu función de JavaScript -->
+        <script>
+            // Función para pintar las estrellas
+            function paintStars(rating) {
+                const stars = document.querySelector('.star-rating');
+                const percentage = (rating / 5) * 100; // Convertir la calificación en un porcentaje
+                console.log(percentage);
+                //stars.style.width = `${percentage}%`; // Establecer el ancho del contenedor de estrellas
+            }
+            // Obtener el promedio de calificación del contenedor y pintar las estrellas
+            const averageRating = parseFloat(document.querySelector('.star-rating').getAttribute('data-rating'));
+            paintStars(averageRating);
+        </script>
 
         </section>
 
