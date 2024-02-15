0e0a399c564523ed799a22054f7a21e394873cb7
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-02-15 10:38:16 -0400

    nuevos archivos en partials

---
+++ resources/views/page/partials/paleta_colores.blade.php
@@ -0,0 +1,13 @@
+<div class="modal fade" id="ModalPaleta" tabindex="-1" aria-labelledby="ModalPaletaLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="ModalPaletaLabel">Paleta de colores</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <img src="{{ asset('images/paleta_colores.jpg') }}" alt="paleta de  colores" style="width: 100%">
+            </div>
+        </div>
+    </div>
+</div>
