c0915d0ab31ec81faae50ed662ca98fe297ad855
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-02-29 10:27:38 -0400

    eliminar script

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -24,15 +24,6 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" async></script>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  
-  <script>
-    function loadModalScript() {
-      var script = document.createElement('script');
-      script.src = 'ruta/modal.js';  // Reemplaza 'ruta/modal.js' con la ubicación correcta
-      script.async = true;
-      document.body.appendChild(script);
-    }
-  </script>
 
   <link rel="stylesheet" href="{{ asset('css/main.css') }}">
   <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
