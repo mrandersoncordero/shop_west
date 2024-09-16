d0b1a3b71ba2b3726e8b2357803daae3616c4044
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-16 15:33:28 -0400

    cambiar el template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -4,18 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{ $metaData['title'] ?? 'Productos Occidente, C.A.' }}</title>
+  <title>@yield('title', 'Productos Occidente, C.A.')</title>
 
   <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
-  <meta name="description" content="{{ $metaData['description'] ?? 'Fabricamos productos de construcción de alta calidad en Venezuela, incluyendo pegamentos, revestimientos y sella juntas. ¡Conozca nuestra línea completa!' }}">
-  <meta name="keywords" content="{{ $metaData['keywords'] ?? 'pegoccidente, pegamento para cerámica, impermeabilizante, porcelanato, exteriores, pego extra fuerte' }}">
-  <link rel="canonical" href="{{ $metaData['canonical'] ?? url()->current() }}">
+
+  <meta name="description" content="@yield('description','Fabricamos productos de construcción de alta calidad en Venezuela, incluyendo pegamentos, revestimientos y sella juntas. ¡Conozca nuestra línea completa!')">
+  <meta name="keywords" content="@yield('keywords', 'pegoccidente, pegamento para cerámica, impermeabilizante, porcelanato, exteriores, pego extra fuerte')">
   <meta property="og:locale" content="es_VE">
   <meta property="og:type" content="website">
-  <meta property="og:title" content="{{ $metaData['og_title'] ?? 'Productos Occidente, C.A.' }}">
-  <meta property="og:description" content="{{ $metaData['og_description'] ?? 'Fabricamos productos de construcción de alta calidad en Venezuela, incluyendo pegamentos, revestimientos y sella juntas. ¡Conozca nuestra línea completa!' }}">
-  <meta property="og:url" content="{{ $metaData['canonical'] ?? url()->current() }}">
-  <meta property="og:site_name" content="Productos Occidente, C.A.">
+
+  <link rel="canonical" href="{{ url()->current() }}">
+  <meta property="og:title" content="@yield('title', 'Productos Occidente, C.A.')">
+  <meta property="og:description" content="@yield('description', 'pegoccidente, pegamento para cerámica, impermeabilizante, porcelanato, exteriores, pego extra fuerte')">
+  <meta property="og:url" content="{{ url()->current() }}">
+  <meta property="og:site_name" content="@yield('title', 'Productos Occidente, C.A.')">
   <meta property="og:image" content="{{ $metaData['og_image'] ?? asset('favicon.ico') }}">
   <meta property="og:image:secure_url" content="{{ asset('favicon.ico') }}">
   <meta property="og:image:type" content="image/x-icon">
